8eb02eed73f62d2ce0a8e016d024e6c2fa26ae0f
overlord <dym8750@gmail.com>
2021-01-04 13:50:06 +0200

    новая страничка thanks

--- mail-send.php
+++ mail-send.php
@@ -30,5 +30,5 @@ $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 mail($to, $subject, $message, $headers);
 
-header('Location: index.html'); 
+header('Location: thanks.html'); 
 ?>
